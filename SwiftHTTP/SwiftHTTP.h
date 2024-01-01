3ee538ac72a71b5b1d4f9a2ba85e9765a6683408
CleverLemming1337 <119337770+CleverLemming1337@users.noreply.github.com>
2024-01-01 01:04:09 +0100

    Initial Commit

---
+++ SwiftHTTP/SwiftHTTP.h
@@ -0,0 +1,18 @@
+//
+//  SwiftHTTP.h
+//  SwiftHTTP
+//
+//  Created by Leonard Fekete on 01.01.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftHTTP.
+FOUNDATION_EXPORT double SwiftHTTPVersionNumber;
+
+//! Project version string for SwiftHTTP.
+FOUNDATION_EXPORT const unsigned char SwiftHTTPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftHTTP/PublicHeader.h>
+
+
---
+++ SwiftHTTPTests/SwiftHTTPTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftHTTPTests.swift
+//  SwiftHTTPTests
+//
+//  Created by Leonard Fekete on 01.01.24.
+//
+
+import Testing
+@testable import SwiftHTTP
+
+struct SwiftHTTPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
