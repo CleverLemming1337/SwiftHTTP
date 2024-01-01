3f7dedeaba06a3ac19cbc78dfc28e1bb17b28897
CleverLemming1337 <119337770+CleverLemming1337@users.noreply.github.com>
2024-01-01 01:04:44 +0100

    Add SwiftHTML

---
+++ SwiftHTML/SwiftHTML.h
@@ -0,0 +1,18 @@
+//
+//  SwiftHTML.h
+//  SwiftHTML
+//
+//  Created by Leonard Fekete on 01.01.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftHTML.
+FOUNDATION_EXPORT double SwiftHTMLVersionNumber;
+
+//! Project version string for SwiftHTML.
+FOUNDATION_EXPORT const unsigned char SwiftHTMLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftHTML/PublicHeader.h>
+
+
